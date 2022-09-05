79206fcecf269b5dfdfba4905f08ee4c0c3dcb8c
Sangeetha1015 <105636319+Sangeetha1015@users.noreply.github.com>
2022-09-05 11:16:44 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,29 +1,26 @@
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# ☛ Req -1 : Install package flask_sqlalchemy
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 from flask_sqlalchemy import SQLAlchemy
 
-
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# ☛ Req -2 : Create db object from SQLAlchemy class
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
+db = SQLAlchemy()
 class Employee(db.Model):
-    __tablename__ = "Employee"
+    table_name = "Employee"
 
-    id = db.Column(db.Integer, primary_key=True)
+    # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    # ☛ Req -2 : Create db object from SQLAlchemy class
+    # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    # id = db.Column(db.Integer)
     # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
     # ☛ Req -3 : Define column employee_id with unique key constraint, this should be of type integer
     # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    employee_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
     age = db.Column(db.Integer())
     position = db.Column(db.String())
 
-    def __init__(self, employee_id, name, age, position):
+    def init(self, employee_id, name, age, position):
         self.employee_id = employee_id
         self.name = name
         self.age = age
         self.position = position
 
-    def __repr__(self):
+    def repr(self):
         return f"{self.name}:{self.employee_id}"
